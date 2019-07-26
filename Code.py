ebc6830d701c506162e8cd419a953180d92bce89
Adamhyw <adamhyw@163.com>
2019-07-26 09:11:14 +0800

    Update Code.py

--- Code.py
+++ Code.py
@@ -7,7 +7,7 @@ def conse_int(num):
 		# 一个数M若可以写成以a开头的连续n个自然数之和，则M=a+(a+1)+(a+2)+…+(a+n-1)=n*a+n*(n-1)/2
 		for start_num in range(1,math.ceil(n/2),1):
 			for conse_num in range(2,math.ceil(n/2)+1,1):
-				if (NUM == start_num * conse_num + conse_num * (conse_num-1)/2):
+				if (n == start_num * conse_num + conse_num * (conse_num-1)/2):
 					satis = 1
 					break
 			if satis == 1:
